67ac78823644671b4984970f5563c549f7e49b03
Freddie Hunt <64798401+huntfreddie404@users.noreply.github.com>
2020-05-04 19:10:38 +0200

    Deel 3 van de code

---
+++ verschillende delen (code)/deel 3.js
@@ -0,0 +1,6 @@
+// Deel 3
+let nameStudent = "Smart teachers has left the chat.";
+document.getElementById("student_label").style.display = "block";
+document.getElementById("student").style.display = "block";
+document.getElementById("student").value = nameStudent;
+console.clear();
